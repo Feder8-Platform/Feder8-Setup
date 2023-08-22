220d8f7919fd48fe480d4b13ab8f2c63eb34df77
Shawn <35766067+ShawnRG@users.noreply.github.com>
2023-08-22 15:33:38 +0200

    Updated script uuid for DQP

--- LupusNet/DataQualityPipeline/run-data-quality-pipeline.cmd
+++ LupusNet/DataQualityPipeline/run-data-quality-pipeline.cmd
@@ -12,4 +12,4 @@ docker login %REGISTRY%
 
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=lupus --env INDICATION=lupus --env CDM_VERSION=5.4 --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=17b8f1e7-d352-417b-82fc-2d0a4f75cdd8 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=lupus --env INDICATION=lupus --env CDM_VERSION=5.4 --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
