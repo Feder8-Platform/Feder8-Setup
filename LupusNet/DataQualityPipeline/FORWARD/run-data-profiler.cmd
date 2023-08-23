b9be3144c961c749215eac1bf4d044fb2095c0f0
Shawn <35766067+ShawnRG@users.noreply.github.com>
2023-08-23 19:35:40 +0200

    Update run-data-profiler.cmd

--- LupusNet/DataQualityPipeline/FORWARD/run-data-profiler.cmd
+++ LupusNet/DataQualityPipeline/FORWARD/run-data-profiler.cmd
@@ -4,4 +4,4 @@ SET TAG=latest
 
 docker pull harbor.lupusnet.org/distributed-analytics/data-profiler:%TAG%
 
-docker run --rm --name data-profiler -v shared:/var/lib/shared -v %CD%/data-profiler-results:/script/data_profiler_results --env THERAPEUTIC_AREA=lupus --env CDM_VERSION=5.4 --env INDICATION=lupus --SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 --env LOG_LEVEL=DEBUG --network feder8-net harbor.lupusnet.org/distributed-analytics/data-profiler:%TAG%
+docker run --rm --name data-profiler -v shared:/var/lib/shared -v %CD%/data-profiler-results:/script/data_profiler_results --env THERAPEUTIC_AREA=lupus --env CDM_VERSION=5.4 --env INDICATION=lupus --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 --env LOG_LEVEL=DEBUG --network feder8-net harbor.lupusnet.org/distributed-analytics/data-profiler:%TAG%
