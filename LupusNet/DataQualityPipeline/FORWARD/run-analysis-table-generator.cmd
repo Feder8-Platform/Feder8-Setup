fff7f6bb945f0a28b957888ff7d640cf14e0309d
Shawn <35766067+ShawnRG@users.noreply.github.com>
2023-08-24 19:33:41 +0200

    Update run-analysis-table-generator.cmd

--- LupusNet/DataQualityPipeline/FORWARD/run-analysis-table-generator.cmd
+++ LupusNet/DataQualityPipeline/FORWARD/run-analysis-table-generator.cmd
@@ -4,4 +4,4 @@ SET TAG=latest
 
 docker pull harbor.lupusnet.org/distributed-analytics/analysis-table-generator-lupus-forward:%TAG%
 
-docker run --rm --name analysis-table-generator -v shared:/var/lib/shared --env THERAPEUTIC_AREA=lupus --env CDM_VERSION=5.4 --env INDICATION=lupus --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 --env LOG_LEVEL=DEBUG --network harbor.lupusnet.org/distributed-analytics/analysis-table-generator-lupus-forward:%TAG%
+docker run --rm --name analysis-table-generator -v shared:/var/lib/shared --env THERAPEUTIC_AREA=lupus --env CDM_VERSION=5.4 --env INDICATION=lupus --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 --env LOG_LEVEL=DEBUG --network feder8-net harbor.lupusnet.org/distributed-analytics/analysis-table-generator-lupus-forward:%TAG%
