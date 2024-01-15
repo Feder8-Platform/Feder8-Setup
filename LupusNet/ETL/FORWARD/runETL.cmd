8ce67ea417ec794dc518ddbbf5effc68a18ec818
Peter Moorthamer <moorthamer@gmail.com>
2024-01-15 17:36:29 +0100

    Update run script to use the ETL runner (JIRA AEKR-1992)

--- LupusNet/ETL/FORWARD/runETL.cmd
+++ LupusNet/ETL/FORWARD/runETL.cmd
@@ -1,21 +1,21 @@
 @echo off
 
-set "data_folder=%CD%/data"
-set /p "data_folder=Input Data folder [%data_folder%]: "
+SET REGISTRY=harbor.honeur.org
+SET REPOSITORY=library
+SET IMAGE=etl-runner
+SET VERSION=1.1.2
+SET TAG=%VERSION%
 
-set "reports_folder=%CD%/reports"
+SET LOG_FOLDER_HOST=%CD%/log
+SET DATA_FOLDER_HOST=%CD%/data
+SET QA_FOLDER_HOST=%CD%/qa
 
-set "db_username=feder8_admin"
-set /p "db_username=DB username [%db_username%]: "
-
-set "db_password="
-set /p "db_password=DB password: "
-
-SET REGISTRY=harbor.lupusnet.org
-SET REPOSITORY=etl-forward
-SET IMAGE=etl
-SET TAG=latest
-
+echo "Pull ETL runner image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --network feder8-net --env DB_USER=%db_username% --env DB_PASSWORD=%db_password% -v %data_folder%:/script/etl/data -v %reports_folder%:/script/etl/forward/reports %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+curl -fsSL https://raw.githubusercontent.com/Feder8-Platform/Feder8-Setup/main/LupusNet/ETL/Forward/questions.json --output questions.json
+
+echo "Run ETL"
+docker run -it --rm --name etl-runner --network feder8-net --env THERAPEUTIC_AREA=lupus --env REGISTRY=harbor.lupusnet.org --env LOG_LEVEL=INFO --env LOG_FOLDER_HOST=%LOG_FOLDER_HOST% --env LOG_FOLDER=/script/etl/forward/log --env ETL_IMAGE_NAME=etl-forward/etl --env ETL_IMAGE_TAG=latest --env DATA_FOLDER_HOST=%DATA_FOLDER_HOST% --env DATA_FOLDER=/script/etl/data --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env QA_FOLDER_ETL=/script/etl/forward/reports --env RUN_DQD=false -v /var/run/docker.sock:/var/run/docker.sock -v %CD%/questions.json:/script/questions.json %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+
+echo "End of ETL run"
---
+++ LupusNet/ETL/FORWARD/runETL.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -ex
+
+REGISTRY=harbor.honeur.org
+REPOSITORY=library
+IMAGE=etl-runner
+VERSION=1.1.2
+TAG=$VERSION
+
+LOG_FOLDER_HOST=${PWD}/log
+DATA_FOLDER_HOST=${PWD}/data
+QA_FOLDER_HOST=${PWD}/qa
+
+echo "Pull ETL runner image"
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+#echo "Download ETL questions"
+curl -fsSL https://raw.githubusercontent.com/Feder8-Platform/Feder8-Setup/main/LupusNet/ETL/Forward/questions.json --output questions.json
+
+touch etl-runner.env
+echo "THERAPEUTIC_AREA=lupus" >> etl-runner.env
+echo "REGISTRY=harbor.lupusnet.org" >> etl-runner.env
+echo "LOG_LEVEL=INFO" >> etl-runner.env
+echo "LOG_FOLDER_HOST=$LOG_FOLDER_HOST" >> etl-runner.env
+echo "LOG_FOLDER=/script/etl/forward/log" >> etl-runner.env
+echo "ETL_IMAGE_NAME=etl-forward/etl" >> etl-runner.env
+echo "ETL_IMAGE_TAG=latest" >> etl-runner.env
+echo "DATA_FOLDER_HOST=$DATA_FOLDER_HOST" >> etl-runner.env
+echo "DATA_FOLDER=/script/etl/data" >> etl-runner.env
+echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> etl-runner.env
+echo "QA_FOLDER_ETL=/script/etl/forward/reports" >> etl-runner.env
+
+echo "RUN_DQD=false" >> etl-runner.env
+
+echo "Run ETL"
+docker run \
+-it \
+--rm \
+--name etl-runner \
+--env-file etl-runner.env \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v ${PWD}/questions.json:/script/questions.json \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "End of ETL run"
+rm -rf etl-runner.env
