0d2002478a681052a9045e59ddee6a83862c2611
Peter Moorthamer <moorthamer@gmail.com>
2024-07-29 15:52:30 +0200

    Add cmd script to convert the source file to the expected format (JIRA AEKR-2088)

---
+++ LupusNet/ETL/APLC/convert-source.cmd
@@ -0,0 +1,13 @@
+@ECHO off
+
+SET REGISTRY=harbor.lupusnet.org
+SET REPOSITORY=etl-aplc
+SET IMAGE=src-converter
+SET VERSION=1.0.0
+SET TAG=%VERSION%
+
+docker login %REGISTRY%
+
+docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+
+docker run --rm --env INPUT_FOLDER=/script/input --env INPUT_FILE=/script/input/aplc.csv --env DELIMITER=, -v %CD%/data:/script/input %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
