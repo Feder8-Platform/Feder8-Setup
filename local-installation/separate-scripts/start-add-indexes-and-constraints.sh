8f13eb78493328ea9b35005fa0dd1aa88be3b6e1
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2024-09-06 10:28:55 +0200

    [feature/AEKR-2096] script start-add-indexes-and-constraints

--- local-installation/separate-scripts/start-add-indexes-and-constraints.sh
+++ local-installation/separate-scripts/start-add-indexes-and-constraints.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-TAG=testing
-#TAG=2.1.3
+TAG=2.1.3
 REGISTRY=harbor.honeur.org
 
-#docker pull ${REGISTRY}/library/install-script:${TAG}
+docker pull ${REGISTRY}/library/install-script:${TAG}
 docker run --rm -it --name feder8-installer -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init add-constraints-and-indexes
+#docker run --rm -it --name feder8-installer -v /var/run/docker.sock:/var/run/docker.sock -v ./feder8-local-images.json:/opt/install-script/feder8-local-images.json feder8/install-script:testing feder8 init add-constraints-and-indexes
+#docker run --rm -it --name feder8-installer -v /var/run/docker.sock:/var/run/docker.sock -v ./feder8-local-images.json:/opt/install-script/feder8-local-images.json ${REGISTRY}/library/install-script:${TAG} feder8 init add-constraints-and-indexes
