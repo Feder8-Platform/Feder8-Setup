214eae349a28e48e9a1524b30a61d8eacd0a6dfa
Peter Moorthamer <moorthamer@gmail.com>
2024-10-25 07:31:06 +0200

    Add SCRIP_UUID env variable (JIRA AEKR-2123)

--- LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
@@ -11,4 +11,4 @@ docker login %REGISTRY%
 echo "Pull image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=741ac115-9504-493c-9ad1-94012f03edb3 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
@@ -13,7 +13,7 @@ echo "Pull image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name lupusnet-rq2-feasibility \
---env THERAPEUTIC_AREA=lupus \
+--env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=741ac115-9504-493c-9ad1-94012f03edb3 \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
