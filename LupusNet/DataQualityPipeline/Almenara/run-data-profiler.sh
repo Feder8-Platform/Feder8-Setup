1eae74fa58585f965ebdb7b5f47e94cac4fced09
Peter Moorthamer <moorthamer@gmail.com>
2024-10-28 08:39:31 +0100

    Remove alternative scripts for Almenara and Forward (JIRA AEKR-2126)

--- LupusNet/DataQualityPipeline/Almenara/run-data-profiler.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-docker run --rm --name data-profiler -v shared:/var/lib/shared -v ${PWD}/data-profiler-results:/script/data_profiler_results --env THERAPEUTIC_AREA=lupus --env CDM_VERSION=5.4 --env INDICATION=lupus --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 --env LOG_LEVEL=DEBUG --network feder8-net harbor.lupusnet.org/distributed-analytics/data-profiler
