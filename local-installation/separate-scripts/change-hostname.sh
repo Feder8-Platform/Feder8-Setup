b81bc41f20179e9f3228e5db4be19e52dcafbcf7
Peter Moorthamer <moorthamer@gmail.com>
2024-11-04 10:53:51 +0100

    Add shell script to change the hostname of the server in the local configuration  (JIRA AEKR-2131)

---
+++ local-installation/separate-scripts/change-hostname.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+REGISTRY=harbor.honeur.org
+REPOSITORY=library
+IMAGE=install-script
+TAG=2.1.4
+
+if systemctl show --property ActiveState docker &> /dev/null; then
+    DOCKER_CERT_SUPPORT=true
+else
+    DOCKER_CERT_SUPPORT=false
+fi
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+  IS_MAC=true
+else
+  IS_MAC=false
+fi
+
+docker pull ${REGISTRY}/${REPOSITORY}/${IMAGE}:${TAG}
+
+docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=${PWD} -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -e ENVIRONMENT=PRD -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/${REPOSITORY}/${IMAGE}:${TAG} feder8 init change-hostname
