e0261a5c7bff235c11512f205108dfd749ad9a71
Peter Moorthamer <moorthamer@gmail.com>
2024-11-22 11:38:08 +0100

    Add primary keys, foreign keys and indexes, so they are reset by the vocabulary update pipeline (JIRA AEKR-2153)

--- local-installation/separate-scripts/upgrade-omopcdm-schema-53-to-54.sh
+++ local-installation/separate-scripts/upgrade-omopcdm-schema-53-to-54.sh
@@ -30,10 +30,37 @@ $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 echo "Set ownership of $NEW_CDM_SCHEMA schema"
 docker exec -it postgres psql -U postgres -d OHDSI -c "REASSIGN OWNED BY feder8_admin TO ohdsi_admin;REASSIGN OWNED BY ohdsi_admin_user TO ohdsi_admin;"
 
+echo "Add primary keys to schema $NEW_CDM_SCHEMA"
+IMAGE=postgres-omopcdm-add-base-primary-keys
+TAG=2.0.3
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+docker run --rm --name  omopcdm-add-base-primary-keys \
+-v shared:/var/lib/shared \
+--env DB_HOST=postgres --env DB_OMOPCDM_SCHEMA=$NEW_CDM_SCHEMA --env CDM_VERSION=5.4 \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "Add foreign keys to schema $NEW_CDM_SCHEMA"
+IMAGE=postgres-omopcdm-add-base-constraints
+TAG=1.0
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+docker run --rm --name  omopcdm-add-base-constraints \
+-v shared:/var/lib/shared \
+--env DB_HOST=postgres --env DB_OMOPCDM_SCHEMA=$NEW_CDM_SCHEMA --env CDM_VERSION=5.4 \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+echo "Add indexes to schema $NEW_CDM_SCHEMA"
+IMAGE=postgres-omopcdm-add-base-indexes
+TAG=2.0.2
+docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+docker run --rm --name  omopcdm-add-base-indexes \
+-v shared:/var/lib/shared \
+--env DB_HOST=postgres --env DB_OMOPCDM_SCHEMA=$NEW_CDM_SCHEMA --env CDM_VERSION=5.4 \
+--network feder8-net \
+$REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
 echo "Load vocabularies in $NEW_CDM_SCHEMA"
 TAG=2.1.4
 docker pull ${REGISTRY}/library/install-script:${TAG}
 docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY="$(pwd)" -e IS_WINDOWS=false -e DOCKER_CERT_SUPPORT=false -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init update-vocabulary -ta honeur
-
-
-
