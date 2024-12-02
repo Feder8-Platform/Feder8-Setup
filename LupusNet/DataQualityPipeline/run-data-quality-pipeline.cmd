66dff7332d33a272e79a790ba7b53e1474747ce6
Peter Moorthamer <moorthamer@gmail.com>
2024-12-02 13:59:03 +0100

    Upgrade DQP run script to use latest version of the Docker image

--- LupusNet/DataQualityPipeline/run-data-quality-pipeline.cmd
+++ LupusNet/DataQualityPipeline/run-data-quality-pipeline.cmd
@@ -3,13 +3,15 @@
 SET REGISTRY=harbor.lupusnet.org
 SET REPOSITORY=distributed-analytics
 SET IMAGE=data-quality-pipeline
-SET VERSION=1.6
+SET VERSION=1.9
 SET TAG=%VERSION%
 SET QA_FOLDER_HOST=%CD%/qa
+SET LOG_FOLDER_HOST=%CD%/logs
+SET LOG_FOLDER=/var/log/dqp
 
 echo "Docker login @ %REGISTRY%"
 docker login %REGISTRY%
 
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=lupus --env INDICATION=lupus --env CDM_VERSION=5.4 --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 -v /var/run/docker.sock:/var/run/docker.sock --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name data-quality-pipeline --env REGISTRY=%REGISTRY% --env THERAPEUTIC_AREA=lupus --env INDICATION=lupus --env CDM_VERSION=5.4 --env QA_FOLDER_HOST=%QA_FOLDER_HOST% --env LOG_FOLDER_HOST=%LOG_FOLDER_HOST% --env LOG_FOLDER=%LOG_FOLDER% --env SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86 -v /var/run/docker.sock:/var/run/docker.sock -v %LOG_FOLDER_HOST%:%LOG_FOLDER% --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- LupusNet/DataQualityPipeline/run-data-quality-pipeline.sh
+++ LupusNet/DataQualityPipeline/run-data-quality-pipeline.sh
@@ -4,14 +4,17 @@ set -ex
 REGISTRY=harbor.lupusnet.org
 REPOSITORY=distributed-analytics
 IMAGE=data-quality-pipeline
-VERSION=1.6
+VERSION=1.9
 TAG=$VERSION
+
+LOG_FOLDER_HOST=${PWD}/logs
+LOG_FOLDER=/var/log/dqp
+QA_FOLDER_HOST=${PWD}/qa
+
 
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
-
-QA_FOLDER_HOST=${PWD}/qa
 
 touch data-quality-pipeline.env
 echo "REGISTRY=$REGISTRY" >> data-quality-pipeline.env
@@ -19,6 +22,8 @@ echo "THERAPEUTIC_AREA=lupus" >> data-quality-pipeline.env
 echo "INDICATION=lupus" >> data-quality-pipeline.env
 echo "CDM_VERSION=5.4"  >> data-quality-pipeline.env
 echo "QA_FOLDER_HOST=$QA_FOLDER_HOST" >> data-quality-pipeline.env
+echo "LOG_FOLDER_HOST=$LOG_FOLDER_HOST" >> data-quality-pipeline.env
+echo "LOG_FOLDER=$LOG_FOLDER" >> data-quality-pipeline.env
 echo "SCRIPT_UUID=82f80336-c3b7-4d09-b325-db8d2965fb86" >> data-quality-pipeline.env
 
 docker run \
@@ -26,6 +31,7 @@ docker run \
 --name data-quality-pipeline \
 --env-file data-quality-pipeline.env \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-v $LOG_FOLDER_HOST:$LOG_FOLDER \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
