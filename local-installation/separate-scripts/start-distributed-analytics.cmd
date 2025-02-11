5d067901d2cb4418d879fec0af100abeeb705464
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2025-02-11 15:15:53 +0100

    [AEKR-2091] Updates after Peter's PR (III)

--- local-installation/separate-scripts/start-distributed-analytics.cmd
+++ local-installation/separate-scripts/start-distributed-analytics.cmd
@@ -7,4 +7,4 @@ set "ORGANIZATION="
 set /p "ORGANIZATION=Enter organization name:"
 
 docker pull %REGISTRY%/library/install-script:%TAG%
-docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=%CD% -e IS_WINDOWS=false -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init full --non_interactive --distributed_analytics --organization=%ORGANIZATION%
+docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=%CD% -e IS_WINDOWS=true -v /var/run/docker.sock:/var/run/docker.sock %REGISTRY%/library/install-script:%TAG% feder8 init full --non_interactive --distributed_analytics --organization=%ORGANIZATION%
