40ba49b15779373cc86ee5fdab38c01f077adaa9
Peter Moorthamer <moorthamer@gmail.com>
2025-04-02 09:54:00 +0200

    Update script version UUID for release v1.4 of LupusNet RQ2 script (JIRA AEKR-2203)

--- LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
@@ -11,4 +11,4 @@ docker login %REGISTRY%
 echo "Pull image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=c14d388e-a709-40d2-a8fb-ec24ac47059f -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=5fe2e29e-88ba-45c0-9c0b-60a626cc1867 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
@@ -13,7 +13,7 @@ echo "Pull image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name lupusnet-rq2-feasibility \
---env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=c14d388e-a709-40d2-a8fb-ec24ac47059f \
+--env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=5fe2e29e-88ba-45c0-9c0b-60a626cc1867 \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
