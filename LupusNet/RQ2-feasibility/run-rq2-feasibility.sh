2ba265e8f90c38179f3f9ed3e40be25acbc28823
Peter Moorthamer <moorthamer@gmail.com>
2025-04-17 16:50:59 +0200

    Release v1.4.4 of LupusNet RQ2 script

--- LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
@@ -4,7 +4,7 @@ set -eux
 REGISTRY=harbor.lupusnet.org
 REPOSITORY=distributed-analytics
 IMAGE=rq2-feasibility
-TAG=1.4.3
+TAG=1.4.4
 
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
@@ -13,7 +13,7 @@ echo "Pull image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name lupusnet-rq2-feasibility \
---env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=1b9387a2-cd4e-4cbe-9d39-0c03477db82c \
+--env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=ffa9e2f5-7697-4fd9-8076-7feac6235553 \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
