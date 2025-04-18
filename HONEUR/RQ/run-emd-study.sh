6c193fbf7664c2886d029a4329444823d84db2c8
Peter Moorthamer <moorthamer@gmail.com>
2025-04-18 10:00:44 +0200

    Update run script for the EMD study (JIRA AEKR-2198)

--- HONEUR/RQ/run-emd-study.sh
+++ HONEUR/RQ/run-emd-study.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=script
 IMAGE=emd-study
-TAG=20250414
+TAG=20250417
 
 echo "Log in to Harbor"
 docker login $REGISTRY
@@ -14,7 +14,7 @@ docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 docker run --rm --name emd-study \
 --env THERAPEUTIC_AREA=HONEUR \
 --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=a466bad7-0f22-4012-8451-1cffce92b41d \
+--env SCRIPT_UUID=cf90f1e1-5cb4-4c70-b380-8278c1e5b950 \
 --network feder8-net \
 -v $PWD/results/EMD:/script/results \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
