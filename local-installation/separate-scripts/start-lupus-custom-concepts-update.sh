c1cf074a7da45e70c5725c9f675ae1110b0bf4b8
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2025-04-25 11:30:54 +0200

    [AEKR-2211] modifications after Peter's review (I)

--- local-installation/separate-scripts/start-lupus-custom-concepts-update.sh
+++ local-installation/separate-scripts/start-lupus-custom-concepts-update.sh
@@ -2,19 +2,9 @@
 
 docker pull harbor.lupusnet.org/lupus/postgres-omopcdm-update-custom-concepts-lupus:latest
 
-touch omopcdm-update-custom-concepts.env
-
-echo "DB_HOST=postgres" >> omopcdm-update-custom-concepts.env
-echo "DB_PORT=5432" >> omopcdm-update-custom-concepts.env
-echo "DB_DATABASE_NAME=OHDSI" >> omopcdm-update-custom-concepts.env
-echo "DB_OMOPCDM_SCHEMA=omopcdm54" >> omopcdm-update-custom-concepts.env
-
 docker run \
 --rm \
 --name omopcdm-update-custom-concepts \
 -v shared:/var/lib/shared \
---env-file omopcdm-update-custom-concepts.env \
 --network feder8-net \
 harbor.lupusnet.org/lupus/postgres-omopcdm-update-custom-concepts-lupus:latest
-
-rm -rf omopcdm-update-custom-concepts.env
