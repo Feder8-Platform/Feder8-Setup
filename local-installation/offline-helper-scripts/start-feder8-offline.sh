924ef7c3557b22b79bd0de9a1c0d84b4db92bdc4
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2025-04-30 14:44:24 +0200

    [AEKR-2216/2212/1545] changing version tag

--- local-installation/offline-helper-scripts/start-feder8-offline.sh
+++ local-installation/offline-helper-scripts/start-feder8-offline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TAG=2.2.3
+TAG=2.2.4
 PYTHON_VERSION=3.11
 REGISTRY=harbor.honeur.org
 
--- local-installation/separate-scripts/start-distributed-analytics.cmd
+++ local-installation/separate-scripts/start-distributed-analytics.cmd
@@ -1,6 +1,6 @@
 @ECHO off
 
-SET TAG=2.2.3
+SET TAG=2.2.4
 SET REGISTRY=harbor.honeur.org
 
 set "ORGANIZATION="
