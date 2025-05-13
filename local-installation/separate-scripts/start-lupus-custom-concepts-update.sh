81942cb4de4b23dd235e673aed7dd23ddeb60a47
Peter Moorthamer <moorthamer@gmail.com>
2025-05-13 13:28:34 +0200

    Add missing env variables

--- local-installation/separate-scripts/start-lupus-custom-concepts-update.sh
+++ local-installation/separate-scripts/start-lupus-custom-concepts-update.sh
@@ -2,9 +2,8 @@
 
 docker pull harbor.lupusnet.org/lupus/postgres-omopcdm-update-custom-concepts-lupus:latest
 
-docker run \
---rm \
---name omopcdm-update-custom-concepts \
+docker run --rm --name omopcdm-update-lupus-custom-concepts \
+--env DB_HOST=postgres --env DB_VOCAB_SCHEMA=omopcdm \
 -v shared:/var/lib/shared \
 --network feder8-net \
 harbor.lupusnet.org/lupus/postgres-omopcdm-update-custom-concepts-lupus:latest
