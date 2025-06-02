8b5ca37259b61768041afb29aa338f7711c07c36
Peter Moorthamer <moorthamer@gmail.com>
2025-06-02 13:22:56 +0200

    Adapt run script to write to a local log file

--- HONEUR/RQ/run-periodic-study.sh
+++ HONEUR/RQ/run-periodic-study.sh
@@ -4,17 +4,20 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=script
 IMAGE=periodic-study
-VERSION=20250523
+VERSION=20250602
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
+echo "" >> $PWD/logofcode.txt
+
 start=$(date +%s)
 
-docker run --rm --name periodic-study \
+docker run --name periodic-study \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
 --env SCRIPT_UUID=8e3be610-1109-42f7-aa58-2439c85abdab \
 -v $PWD/results:/script/results \
+-v $PWD/logofcode.txt:/script/logofcode.txt \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
