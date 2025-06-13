c0cc0555cda978ddd7d4452e2cb2d0d13bc01640
silviajimeneznavarro-edence <silvia.jimeneznavarro@edence.health>
2025-06-13 15:01:18 +0200

    [AEKR-2251] changing DOCKER_HOST env variable

--- local-installation/helper-scripts/start-feder8.sh
+++ local-installation/helper-scripts/start-feder8.sh
@@ -3,7 +3,7 @@
 TAG=2.2.4
 REGISTRY=harbor.honeur.org
 
-DOCKER_HOST="${DOCKER_HOST:-172.17.0.1}"
+DOCKER_HOST_CONFIG="${DOCKER_HOST_CONFIG:-172.17.0.1}"
 
 if systemctl show --property ActiveState docker &> /dev/null; then
     DOCKER_CERT_SUPPORT=true
@@ -18,4 +18,4 @@ else
 fi
 
 docker pull ${REGISTRY}/library/install-script:${TAG}
-docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -e DOCKER_HOST=$DOCKER_HOST -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init full
+docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -e DOCKER_HOST_CONFIG=$DOCKER_HOST_CONFIG -v /var/run/docker.sock:/var/run/docker.sock ${REGISTRY}/library/install-script:${TAG} feder8 init full
