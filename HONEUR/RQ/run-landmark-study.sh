e0609fef01c77c2fbfd6c1358c88dfdaa2c41f93
Peter Moorthamer <moorthamer@gmail.com>
2025-06-20 13:45:04 +0200

    Release v9 of Landmark study script (JIRA AEKR-2195)

--- HONEUR/RQ/run-landmark-study.sh
+++ HONEUR/RQ/run-landmark-study.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_36
 IMAGE=landmark-study
-VERSION=V8
+VERSION=V9
 TAG=$VERSION
 
 echo "Docker login @ $REGISTRY"
@@ -14,7 +14,7 @@ echo "Pull Docker image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name landmark-study \
---env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=83a96536-3a88-4400-9b31-bf94ba45a21e \
+--env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=c10fcc29-ad17-42ec-893d-35638ae4b989 \
 -v "$PWD/results":/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
