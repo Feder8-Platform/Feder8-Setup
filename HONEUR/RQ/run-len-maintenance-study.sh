32bb1c58f7977fb106014291bef67fbaacccb781
Peter Moorthamer <moorthamer@gmail.com>
2025-08-05 17:56:00 +0200

    Update version of len Maintenance study script to 20250805

--- HONEUR/RQ/run-len-maintenance-study.sh
+++ HONEUR/RQ/run-len-maintenance-study.sh
@@ -4,14 +4,14 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=script
 IMAGE=len-maintenance-study
-VERSION=20250702
+VERSION=20250805
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name len-maintenance-study \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=27de39d8-9fb9-444e-bb70-46700ab0dc2f \
+--env SCRIPT_UUID=189a1d9a-1d27-4dc0-b26f-f01476d6762b \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
