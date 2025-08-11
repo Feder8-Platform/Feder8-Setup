233aef763bf2792d341b0e8c7f37d394efa81aa1
Peter Moorthamer <moorthamer@gmail.com>
2025-08-11 23:03:17 +0200

    Update run script for BiTeS study

--- HONEUR/RQ/run-bites-study.sh
+++ HONEUR/RQ/run-bites-study.sh
@@ -12,7 +12,7 @@ docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 docker run --rm --name bites-study \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
 --env SCRIPT_UUID=27067510-c75e-4b0d-a0a3-751fd8854581 \
--v "$PWD/results":/script/results \
+-v "$PWD/results/bites":/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
