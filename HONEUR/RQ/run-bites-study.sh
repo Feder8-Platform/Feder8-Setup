6f4b70629ecd67f4e480aff721873b673150d178
Peter Moorthamer <moorthamer@gmail.com>
2025-09-17 17:35:26 +0200

    Release new version of BITES study script

--- HONEUR/RQ/run-bites-study.sh
+++ HONEUR/RQ/run-bites-study.sh
@@ -4,14 +4,14 @@ set -eux
 REGISTRY=harbor.honeur.org
 REPOSITORY=script
 IMAGE=bites-study
-VERSION=20250808
+VERSION=20250915
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name bites-study \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=27067510-c75e-4b0d-a0a3-751fd8854581 \
+--env SCRIPT_UUID=6327f043-f81a-45e2-8dcf-6cc7faf96e34 \
 -v "$PWD/results/bites":/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
