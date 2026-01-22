205e8a334da66ccc67853f0d5f0b0dae927d2986
Peter Moorthamer <moorthamer@gmail.com>
2026-01-22 11:11:51 +0100

    [AEKR-2308] update run script for PERIODIC study

--- HONEUR/RQ/run-periodic-study.sh
+++ HONEUR/RQ/run-periodic-study.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_49
 IMAGE=periodic-study
-VERSION=V12
+VERSION=V13
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
@@ -13,7 +13,7 @@ start=$(date +%s)
 
 docker run --name periodic-study-$start \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=ecc4942d-1cf1-41bb-892d-5755dd801231 \
+--env SCRIPT_UUID=6e2b598e-f1fe-47b7-a614-9e9a2e90f021 \
 -v $PWD/results:/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
