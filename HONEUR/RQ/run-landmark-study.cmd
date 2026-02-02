d7982d6c715dee0044834b20570d49c1c21851ea
Peter Moorthamer <moorthamer@gmail.com>
2026-02-02 16:23:53 +0100

    Update Windows run script for the LANDMARK study

--- HONEUR/RQ/run-landmark-study.cmd
+++ HONEUR/RQ/run-landmark-study.cmd
@@ -1,9 +1,9 @@
 @ECHO off
 
 SET REGISTRY=harbor.honeur.org
-SET REPOSITORY=study_36
+SET REPOSITORY=script
 SET IMAGE=landmark-study
-SET VERSION=V13
+SET VERSION=20260202
 SET TAG=%VERSION%
 
 echo "Docker login @ %REGISTRY%"
@@ -12,5 +12,7 @@ docker login %REGISTRY%
 echo "Pull Docker image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name landmark-study --env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=9d91de06-c135-4eaf-b0ec-ae3ab3dc6c74 -v "%CD%/results":/script/results -v feder8-data:/home/feder8/data --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+type nul > "%CD%\logofcode.txt"
+
+docker run --rm --name landmark-study --env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=9d91de06-c135-4eaf-b0ec-ae3ab3dc6c74 -v "%CD%/logofcode.txt":/script/logofcode.txt -v "%CD%/results":/script/results -v feder8-data:/home/feder8/data --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
