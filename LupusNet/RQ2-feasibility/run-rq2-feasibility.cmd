3dfc519e30b00c922f1b71bfa5841224d402f430
Peter Moorthamer <moorthamer@gmail.com>
2026-02-11 13:24:30 +0100

    Update LupusNet RQ2 run script to v1.5.14

--- LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.cmd
@@ -3,7 +3,7 @@
 SET REGISTRY=harbor.lupusnet.org
 SET REPOSITORY=distributed-analytics
 SET IMAGE=rq2-feasibility
-SET TAG=1.5.13
+SET TAG=1.5.14
 
 echo "Docker login @ %REGISTRY%"
 docker login %REGISTRY%
@@ -12,4 +12,4 @@ echo "Pull image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
 docker rm -f lupusnet-rq2-feasibility 2>nul
-docker run --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=bc613862-4237-45ef-b53a-23fcf89a1d38 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --name lupusnet-rq2-feasibility --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=2cc5976d-d92c-4f79-a1e3-89dd47c44c6b -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
+++ LupusNet/RQ2-feasibility/run-rq2-feasibility.sh
@@ -4,7 +4,7 @@ set -eux
 REGISTRY=harbor.lupusnet.org
 REPOSITORY=distributed-analytics
 IMAGE=rq2-feasibility
-TAG=1.5.13
+TAG=1.5.14
 
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
@@ -15,7 +15,7 @@ docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 docker rm -f lupusnet-rq2-feasibility 2>/dev/null || true
 
 docker run --name lupusnet-rq2-feasibility \
---env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=bc613862-4237-45ef-b53a-23fcf89a1d38 \
+--env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=2cc5976d-d92c-4f79-a1e3-89dd47c44c6b \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
