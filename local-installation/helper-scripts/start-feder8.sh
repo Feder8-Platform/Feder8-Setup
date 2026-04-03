d07ae2895f07debdefe7eb4ded62ce026b9182fd
Peter Moorthamer <moorthamer@gmail.com>
2026-04-03 11:19:02 +0200

    Fix docker-compose volume mapping

--- local-installation/helper-scripts/start-feder8.sh
+++ local-installation/helper-scripts/start-feder8.sh
@@ -18,4 +18,4 @@ else
 fi
 
 docker pull ${REGISTRY}/library/install-script:${TAG}
-docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -e DOCKER_HOST_CONFIG=$DOCKER_HOST_CONFIG -v /var/run/docker.sock:/var/run/docker.sock -v ./info_feder8_installation:/opt/install-script/info_feder8_installation ${REGISTRY}/library/install-script:${TAG} feder8 init full
+docker run --rm -it --name feder8-installer -e CURRENT_DIRECTORY=$(pwd) -e IS_WINDOWS=false -e IS_MAC=$IS_MAC -e DOCKER_CERT_SUPPORT=$DOCKER_CERT_SUPPORT -e DOCKER_HOST_CONFIG=$DOCKER_HOST_CONFIG -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/info_feder8_installation:/opt/install-script/info_feder8_installation ${REGISTRY}/library/install-script:${TAG} feder8 init full
