1dfd1acb7041ef1c8b29a51067a840aec13c27e5
Peter Moorthamer <moorthamer@gmail.com>
2026-04-15 09:31:56 +0200

    Grant required DB permissions after creating the analysis table

--- LupusNet/AnalysisTable/generate-analysis-table.sh
+++ LupusNet/AnalysisTable/generate-analysis-table.sh
@@ -20,3 +20,6 @@ docker run \
 --env DB_ANALYSIS_TABLE_NAME=analysis_table \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
+
+docker exec -it postgres psql -U postgres -d OHDSI -c "REASSIGN OWNED BY feder8_admin TO ohdsi_admin;GRANT USAGE ON SCHEMA results TO ohdsi_app;GRANT SELECT ON ALL TABLES IN SCHEMA results TO ohdsi_app;"
+
