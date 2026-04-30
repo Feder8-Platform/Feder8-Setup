338e1476ee964a09f4410a55488d3974806f0e9b
Peter Moorthamer <moorthamer@gmail.com>
2026-04-30 13:23:05 +0200

    [AEKR-2386] Update run script for the BITES study

--- HONEUR/RQ/run-bites-study.cmd
+++ HONEUR/RQ/run-bites-study.cmd
@@ -3,7 +3,7 @@
 SET REGISTRY=harbor.honeur.org
 SET REPOSITORY=script
 SET IMAGE=bites-study
-SET VERSION=20251009
+SET VERSION=20260416
 SET TAG=%VERSION%
 
 echo "Docker login @ %REGISTRY%"
@@ -12,4 +12,4 @@ docker login %REGISTRY%
 echo "Pull Docker image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name bites-study --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table --env SCRIPT_UUID=9e9649a5-93b7-47e9-9425-d92a02a2304f -v "%CD%/results/bites":/script/results -v feder8-data:/home/feder8/data --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --rm --name bites-study --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table --env SCRIPT_UUID=f5386fc0-eafb-4a2d-ad97-c4f1789896e9 -v "%CD%/results/bites":/script/results -v feder8-data:/home/feder8/data --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- HONEUR/RQ/run-bites-study.sh
+++ HONEUR/RQ/run-bites-study.sh
@@ -4,14 +4,14 @@ set -eux
 REGISTRY=harbor.honeur.org
 REPOSITORY=script
 IMAGE=bites-study
-VERSION=20251009
+VERSION=20260416
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name bites-study \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=9e9649a5-93b7-47e9-9425-d92a02a2304f \
+--env SCRIPT_UUID=f5386fc0-eafb-4a2d-ad97-c4f1789896e9 \
 -v "$PWD/results/bites":/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
