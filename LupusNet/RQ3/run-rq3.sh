c8655933e5f65fd162a9819725151dc25dd45cb5
Peter Moorthamer <moorthamer@gmail.com>
2026-05-18 11:51:52 +0200

    Update run script for LupusNet RQ3 feasibility (memory optimizations)

--- LupusNet/RQ3/run-rq3.sh
+++ LupusNet/RQ3/run-rq3.sh
@@ -4,7 +4,7 @@ set -eux
 REGISTRY=harbor.lupusnet.org
 REPOSITORY=distributed-analytics
 IMAGE=rq3-feasibility
-TAG=1.0.0
+TAG=1.0.1
 
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
@@ -13,7 +13,10 @@ echo "Pull image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name lupusnet-rq3 \
---env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=9ca7c005-1a6c-4908-9e37-4079e7c07991 \
+--memory-swap -1 \
+--env THERAPEUTIC_AREA=lupus \
+--env ANDROMEDA_TEMP_FOLDER=/tmp/andromeda \
+--env SCRIPT_UUID=9ca7c005-1a6c-4908-9e37-4079e7c07991 \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
