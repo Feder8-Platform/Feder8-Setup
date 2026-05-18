df5f6607ac30368ba58cfced5d84cb1ccc241faa
Peter Moorthamer <moorthamer@gmail.com>
2026-05-18 13:17:51 +0200

    Update cmd run script for LupusNet RQ3 feasibility (memory optimizations)

--- LupusNet/RQ3/run-rq3.cmd
+++ LupusNet/RQ3/run-rq3.cmd
@@ -11,4 +11,4 @@ docker login %REGISTRY%
 echo "Pull image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --rm --name lupusnet-rq3 --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=9ca7c005-1a6c-4908-9e37-4079e7c07991 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --name lupusnet-rq3 --memory-swap -1 --env ANDROMEDA_TEMP_FOLDER=/tmp/andromeda --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=9ca7c005-1a6c-4908-9e37-4079e7c07991 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
