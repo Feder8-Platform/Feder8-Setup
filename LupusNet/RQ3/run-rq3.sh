99486d47d3f94699ef3ca6dbf4ef4dfcdf89feb2
Peter Moorthamer <moorthamer@gmail.com>
2026-05-27 09:01:17 +0200

    Add mapping for errorReportSql.txt to debug error at Almenara

--- LupusNet/RQ3/run-rq3.sh
+++ LupusNet/RQ3/run-rq3.sh
@@ -12,11 +12,15 @@ docker login $REGISTRY
 echo "Pull image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
+mkdir -p $PWD/results
+touch $PWD/results/errorReportSql.txt
+
 docker run --rm --name lupusnet-rq3 \
 --memory-swap -1 \
 --env THERAPEUTIC_AREA=lupus \
 --env ANDROMEDA_TEMP_FOLDER=/tmp/andromeda \
 --env SCRIPT_UUID=9ca7c005-1a6c-4908-9e37-4079e7c07991 \
 -v $PWD/results:/script/results \
+-v $PWD/results/errorReportSql.txt:/errorReportSql.txt \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
