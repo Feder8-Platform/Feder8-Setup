6f5ca7428c09855679e650fc07bc6cd9102e83d8
Peter Moorthamer <moorthamer@gmail.com>
2026-06-01 17:38:18 +0200

    Update EMD study script

--- HONEUR/RQ/run-emd-study.sh
+++ HONEUR/RQ/run-emd-study.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_47
 IMAGE=emd-study
-TAG=V4
+TAG=V5
 
 echo "Log in to Harbor"
 docker login $REGISTRY
@@ -14,9 +14,10 @@ docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 docker run --rm --name emd-study \
 --env THERAPEUTIC_AREA=HONEUR \
 --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=5799a23f-6945-4dd1-b481-e5d4b092ed00 \
+--env SCRIPT_UUID=d25c3551-96cf-43e0-a2ba-a4aeb90d975f \
 --network feder8-net \
 -v $PWD/results/EMD:/script/results \
+-v feder8-data:/home/feder8/data \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 echo "EMD study executed. Thank you!"
