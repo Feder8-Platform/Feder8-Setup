4fb649a98381cfff46346b4c473b41298ab8a149
Peter Moorthamer <moorthamer@gmail.com>
2026-06-09 15:31:40 +0200

    [AEKR-2396] update run script for the PERIODIC study for ASH2026

--- HONEUR/RQ/run-periodic-study-ash2026.sh
+++ HONEUR/RQ/run-periodic-study-ash2026.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_49
 IMAGE=periodic-study-ash26
-VERSION=V1
+VERSION=V2
 TAG=$VERSION
 
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
@@ -13,7 +13,7 @@ start=$(date +%s)
 
 docker run --name periodic-study-ash26-$start \
 --env THERAPEUTIC_AREA=HONEUR --env DB_ANALYSIS_TABLE_NAME=analysis_table \
---env SCRIPT_UUID=214521eb-7f2c-4516-8969-17f4d354a673 \
+--env SCRIPT_UUID=ffa980ff-0df1-4805-9557-86308b2e10af \
 -v $PWD/results:/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
