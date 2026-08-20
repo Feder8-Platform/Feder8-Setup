f75b9fb8df0dd02b2517c3e4e5483a0de69d0c06
Peter Moorthamer <moorthamer@gmail.com>
2026-08-20 10:12:34 +0200

    Update run script for the Landmark CoxModel

--- HONEUR/RQ/run-landmark-coxmodel.sh
+++ HONEUR/RQ/run-landmark-coxmodel.sh
@@ -4,7 +4,7 @@ set -ex
 REGISTRY=harbor.honeur.org
 REPOSITORY=study_36
 IMAGE=landmark-coxmodel
-VERSION=V2
+VERSION=V3
 TAG=$VERSION
 
 echo "Docker login @ $REGISTRY"
@@ -14,9 +14,8 @@ echo "Pull Docker image"
 docker pull $REGISTRY/$REPOSITORY/$IMAGE:$TAG
 
 docker run --rm --name landmark-study \
---env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=e0acb166-d8af-4a41-928d-d451a1ef086c \
+--env THERAPEUTIC_AREA=HONEUR --env SCRIPT_UUID=fbb63480-c8f0-46ad-859b-383e284ce419 \
 -v "$PWD/results":/script/results \
 -v feder8-data:/home/feder8/data \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
-
