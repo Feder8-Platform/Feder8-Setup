07da3ab403c56d7f9212222ba769e51eab39baec
Peter Moorthamer <moorthamer@gmail.com>
2026-09-14 22:45:28 +0200

    Release simplified version (v1.1.0)

--- LupusNet/RQ3/run-rq3-treatment-trajectories.cmd
+++ LupusNet/RQ3/run-rq3-treatment-trajectories.cmd
@@ -3,7 +3,7 @@
 SET REGISTRY=harbor.lupusnet.org
 SET REPOSITORY=distributed-analytics
 SET IMAGE=rq3-treatment-trajectories
-SET TAG=1.0.0
+SET TAG=1.1.0
 
 echo "Docker login @ %REGISTRY%"
 docker login %REGISTRY%
@@ -11,4 +11,4 @@ docker login %REGISTRY%
 echo "Pull image"
 docker pull %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
 
-docker run --name rq3-treatment-trajectories --memory-swap -1 --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=57d9df9a-e063-45fc-9e70-556dc9e9f6d3 -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
+docker run --name rq3-treatment-trajectories --memory-swap -1 --env THERAPEUTIC_AREA=lupus --env SCRIPT_UUID=0266ef5a-a7d2-44b3-a7cf-f4dcadb6891b -v %CD%/results:/script/results --network feder8-net %REGISTRY%/%REPOSITORY%/%IMAGE%:%TAG%
--- LupusNet/RQ3/run-rq3-treatment-trajectories.sh
+++ LupusNet/RQ3/run-rq3-treatment-trajectories.sh
@@ -4,7 +4,7 @@ set -eux
 REGISTRY=harbor.lupusnet.org
 REPOSITORY=distributed-analytics
 IMAGE=rq3-treatment-trajectories
-TAG=1.0.0
+TAG=1.1.0
 
 echo "Docker login @ $REGISTRY"
 docker login $REGISTRY
@@ -17,7 +17,7 @@ mkdir -p $PWD/results
 docker run --rm --name rq3-treatment-trajectories \
 --memory-swap -1 \
 --env THERAPEUTIC_AREA=lupus \
---env SCRIPT_UUID=57d9df9a-e063-45fc-9e70-556dc9e9f6d3 \
+--env SCRIPT_UUID=0266ef5a-a7d2-44b3-a7cf-f4dcadb6891b \
 -v $PWD/results:/script/results \
 --network feder8-net \
 $REGISTRY/$REPOSITORY/$IMAGE:$TAG
